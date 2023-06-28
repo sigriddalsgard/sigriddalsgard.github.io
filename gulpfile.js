9edc7ff210c63ea2802862d772daf2689c723c73
Sigrið Gyldenkærne Dalsgarð <sigriddalsgard@users.noreply.github.com>
2023-06-28 10:58:50 +0200

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,89 @@
-'use strict';
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
+const del = require("del");
 
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-const del = require('del');
+const config = {
+  size: {
+    showFiles: true
+  }
+};
 
-gulp.task('css', ['css:clean'], function () {
-  return gulp.src('css/*.css')
-    .pipe($.cssnext())
+gulp.task("css:clean", () => del(["dist/css"]));
+
+gulp.task("css", () =>
+  gulp
+    .src("src/css/*.css")
+    .pipe(
+      $.postcss([
+        require("postcss-import")(),
+        require("postcss-url")(),
+        require("postcss-cssnext")()
+      ])
+    )
     .pipe($.cssnano())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("dist/css"))
+    .pipe($.livereload())
+    .pipe($.size({ ...config.size, title: "css" }))
+);
+
+gulp.task("js:clean", () => del(["dist/js"]));
+
+gulp.task("js", () =>
+  gulp
+    .src("src/js/*.js")
+    .pipe(gulp.dest("dist/js"))
+    .pipe($.livereload())
+    .pipe($.size({ ...config.size, title: "js" }))
+);
+
+gulp.task("img:clean", () => del(["dist/img"]));
+
+gulp.task("img", () =>
+  gulp
+    .src("src/img/*.svg")
+    .pipe(gulp.dest("dist/img"))
+    .pipe($.livereload())
+    .pipe($.size({ ...config.size, title: "img" }))
+);
+
+gulp.task("html:clean", () => del(["dist/*.html"]));
+
+gulp.task("html", () =>
+  gulp
+    .src("src/*.html")
+    .pipe($.nunjucks.compile())
+    .pipe(
+      $.htmlmin({
+        removeWhitespace: true
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe($.livereload())
+    .pipe($.size({ ...config.size, title: "html" }))
+);
+
+gulp.task(
+  "clean",
+  gulp.parallel("css:clean", "js:clean", "img:clean", "html:clean")
+);
+
+gulp.task("build", gulp.parallel("css", "js", "img", "html"));
+
+gulp.task("watch", () => {
+  $.livereload.listen();
+
+  gulp.watch("src/**/*.css", gulp.series("css"));
+  gulp.watch("src/**/*.js", gulp.series("js"));
+  gulp.watch("src/**/*.svg", gulp.series("img"));
+  gulp.watch("src/**/*.html", gulp.series("html"));
 });
 
-gulp.task('css:clean', function () {
-  return del(['dist/*.css']);
-});
+gulp.task("serve", () =>
+  gulp.src("dist").pipe(
+    $.webserver({
+      livereload: true
+    })
+  )
+);
 
-gulp.task('html', ['html:clean'], function () {
-  return gulp.src('html/*.html')
-    .pipe($.minifyHtml())
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('html:clean', function () {
-  return del(['dist/*.html']);
-});
-
-gulp.task('img', ['img:clean'], function () {
-  return gulp.src('img/**/*.{jpg,png}')
-    .pipe($.imagemin())
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('img:clean', function () {
-  return del(['dist/**/*.{jpg,png}']);
-});
-
-gulp.task('build', [
-  'css',
-  'html',
-  'img'
-]);
-
-gulp.task('watch', ['build'], function () {
-  gulp.watch('css/**/*.css', ['css']);
-  gulp.watch('html/**/*.html', ['html']);
-});
-
-gulp.task('serve', ['watch'], function () {
-  return gulp.src('dist')
-    .pipe($.webserver({
-      open: true
-    }));
-});
-
-gulp.task('default', ['build']);
-
-gulp.task('deploy', ['deploy:clean', 'build'], function () {
-  return gulp.src('dist/**')
-    .pipe($.ghPages({
-      branch: 'master',
-      cacheDir: '.tmp',
-      force: true
-    }));
-});
-
-gulp.task('deploy:clean', function () {
-  return del(['.tmp']);
-});
+gulp.task("start", gulp.series("build", gulp.parallel("watch", "serve")));
